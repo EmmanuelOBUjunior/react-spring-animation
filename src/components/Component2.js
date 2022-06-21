04ab6de8b2dd37ba551cd59c7e2f8bfdbb8a168d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-21 16:33:09 +0000

    Made changed to component 2

--- src/components/Component2.js
+++ src/components/Component2.js
@@ -3,13 +3,13 @@ import { useSpring, animated } from 'react-spring'
 
 const Component2 = () => {
 	const props = useSpring({
-		to: {opacity: 1, marginTop: 0}, 
-		from: {opacity: 0, marginTop: -500},
-		delay: 500})
+		to: {opacity: 1}, 
+		from: {opacity: 0},
+		delay: 1000})
   return (
 	<animated.div style = {props}>
 		<div style={c1Style}>
-			<h1>Component 1</h1>
+			<h1>Component 2</h1>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem distinctio iste blanditiis, nulla nesciunt sequi rem perferendis! Enim ut eius nam at optio numquam ea, cupiditate expedita voluptate alias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, commodi laudantium odit hic excepturi ea fugit mollitia earum, officia et, vel voluptate accusamus repudiandae perferendis. Ad reprehenderit ex vel sequi.</p>	
 		</div>
 	</animated.div>
@@ -17,7 +17,7 @@ const Component2 = () => {
 }
 
 const c1Style = {
-	background: 'steelblue',
+	background: 'slateblue',
 	color: 'white',
 	padding: '1.5rem'
 }
