57f67f13ef763a5d3260916bb1b705b0dade5bfe
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-21 16:18:06 +0000

    Component1 done

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -4,7 +4,7 @@ const Component1 = () => {
   return (
 		<div style={{c1Style}}>
 			<h1>Component 1</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem distinctio iste blanditiis, nulla nesciunt sequi rem perferendis! Enim ut eius nam at optio numquam ea, cupiditate expedita voluptate alias!</p>	
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem distinctio iste blanditiis, nulla nesciunt sequi rem perferendis! Enim ut eius nam at optio numquam ea, cupiditate expedita voluptate alias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, commodi laudantium odit hic excepturi ea fugit mollitia earum, officia et, vel voluptate accusamus repudiandae perferendis. Ad reprehenderit ex vel sequi.</p>	
 		</div>
   )
 }
