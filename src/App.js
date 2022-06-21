fec1a1cdf1e9ac0318ccac83f8f6d1baaf33c4af
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-06-21 16:17:43 +0000

    Component1 done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Component1 from './components/Component1';
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome</h1>
+     <Component1/>
     </div>
   );
 }
--- src/components/Component1.js
+++ src/components/Component1.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Component1 = () => {
   return (
-	<div>Component1</div>
+		<div style={{c1Style}}>
+			<h1>Component 1</h1>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolorem distinctio iste blanditiis, nulla nesciunt sequi rem perferendis! Enim ut eius nam at optio numquam ea, cupiditate expedita voluptate alias!</p>	
+		</div>
   )
+}
+
+const c1Style = {
+	background: 'steelblue',
+	color: 'white'
 }
 
 export default Component1
